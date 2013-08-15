9679f503eb8b99e191fb12440436e07558a17a1c
Sergey Fukanchik <fukanchik@gmail.com>
2013-08-15 21:15:51 +0400

    code reformat

--- src/java/ru/fuxx/Repack.java
+++ src/java/ru/fuxx/Repack.java
@@ -1,42 +1,39 @@
 package ru.fuxx;
 
+import org.xml.sax.helpers.DefaultHandler;
+
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
-import org.xml.sax.helpers.DefaultHandler;
+public class Repack {
+    private static void usage() {
+        System.out.println("Usage:\nRepack --stat input.xml | --trie | --index | --list input.xml output.txt | --hash titles");
+        System.out.println("\t--list - save list of page titles");
 
-public class Repack
-{
-    private static void usage()
-    {
-	System.out.println("Usage:\nRepack --stat input.xml | --trie | --index | --list input.xml output.txt | --hash titles");
-	System.out.println("\t--list - save list of page titles");
-
-	System.exit(0);
+        System.exit(0);
     }
 
-    public static void main(String [] args) throws Exception
-    {
-	if(0 == args.length) {
-	    usage();
-	}
+    public static void main(String[] args) throws Exception {
+        if (0 == args.length) {
+            usage();
+        }
 
-	final SAXParserFactory factory = SAXParserFactory.newInstance();
-	final SAXParser parser = factory.newSAXParser();
+        final SAXParserFactory factory = SAXParserFactory.newInstance();
+        final SAXParser parser = factory.newSAXParser();
 
-	DefaultHandler handler;
-	String input;
-	if(args[0].equalsIgnoreCase("--list")) {
-	    if(args.length != 3) {
-		usage();
-	    }
-	    input = args[1];
-	    handler = new TitleListHandler(args[2]);
-	} else {
-	    input = null;
-	    handler = null;
-	    usage();
-	}
-	parser.parse(input, handler);
+        DefaultHandler handler;
+        String input;
+        if (args[0].equalsIgnoreCase("--list")) {
+            if (args.length != 3) {
+                usage();
+            }
+            input = args[1];
+            handler = new TitleListHandler(args[2]);
+        } else {
+            input = null;
+            handler = null;
+            usage();
+        }
+        parser.parse(input, handler);
     }
 }
--- src/java/ru/fuxx/TitleListHandler.java
+++ src/java/ru/fuxx/TitleListHandler.java
@@ -1,106 +1,106 @@
 package ru.fuxx;
 
+import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
-import org.xml.sax.Attributes;
 
-import java.io.PrintWriter;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.*;
 
-public class TitleListHandler extends DefaultHandler
-{
+public class TitleListHandler extends DefaultHandler {
     private StringBuilder currentBuffer;
     private String title;
     private String name;
     private Writer w = null;
 
-    public TitleListHandler(final String outName)
-    {
-	this.name = outName;
-    }
-    private void open(final String name)
-    {
-	try {
-	    w = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(name), "UTF-8"));
-	} catch(IOException e) {
-	    e.printStackTrace();
-	    System.exit(-1);
-	} finally {
-	    //if(w!=null)try {w.close();} catch(Exception e){e.printStackTrace();}
-	}
+    public TitleListHandler(final String outName) {
+        this.name = outName;
     }
 
-    private void write(String title)
-    {
-	try {
-	    w.write(title+"\n");
-	} catch(IOException e) {
-	    e.printStackTrace();
-	    System.exit(1);
-	}
+    private void open(final String name) {
+        try {
+            w = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(name), "UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        } finally {
+            //if(w!=null)try {w.close();} catch(Exception e){e.printStackTrace();}
+        }
     }
-    private void close()
-    {
-	try {w.close();} catch(IOException e){e.printStackTrace();}
+
+    private void write(String title) {
+        try {
+            w.write(title + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private void close() {
+        try {
+            w.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void startElement(String uri,
-			     String localName,
-			     String qName,
-			     Attributes attributes)
-	throws SAXException {
-	if(qName.equalsIgnoreCase("mediawiki")) {
-	    open(name);
-	} else if(qName.equalsIgnoreCase("page")) {
-	    ;
-	} else if(qName.equalsIgnoreCase("title")) {
-	    currentBuffer = new StringBuilder();
-	} else if(qName.equalsIgnoreCase("text")) {
-	}
+                             String localName,
+                             String qName,
+                             Attributes attributes)
+            throws SAXException {
+        if (qName.equalsIgnoreCase("mediawiki")) {
+            open(name);
+        } else if (qName.equalsIgnoreCase("page")) {
+            ;
+        } else if (qName.equalsIgnoreCase("title")) {
+            currentBuffer = new StringBuilder();
+        } else if (qName.equalsIgnoreCase("text")) {
+        }
     }
+
     public void endElement(String uri,
-			   String localName,
-			   String qName)
-	throws SAXException {
-	if(qName.equalsIgnoreCase("page")) {
-	    if(title.startsWith("Обсуждение портала:")
-	       ||title.startsWith("Портал:")
-	       ||title.startsWith("Обсуждение категории:")
-	       ||title.startsWith("Категория:")
-	       ||title.startsWith("Обсуждение справки:")
-	       ||title.startsWith("Справка:")
-	       ||title.startsWith("Обсуждение шаблона")
-	       ||title.startsWith("Шаблон:")
-	       ||title.startsWith("Обсуждение MediaWiki:")
-	       ||title.startsWith("MediaWiki:")
-	       ||title.startsWith("Обсуждение файла:")
-	       ||title.startsWith("Файл:")
-	       ||title.startsWith("Обсуждение Википедии:")
-	       ||title.startsWith("Википедия:")
-	       ||title.startsWith("Обсуждение участника:")
-	       ||title.startsWith("Участник:")
-	       ||title.startsWith("Обсуждение:")
-	       ||title.startsWith("Служебная:")
-	       ||title.startsWith("Медиа:"))
-		return;
-	    write(title);
-	    title = null;
-	} else if(qName.equalsIgnoreCase("title")) {
-	    title = currentBuffer.toString();
-	    currentBuffer = null;
-	} else if(qName.equalsIgnoreCase("text")) {
-	    currentBuffer = null;
-	} else if(qName.equalsIgnoreCase("mediawiki")) {
-	    close();
-	}
+                           String localName,
+                           String qName)
+            throws SAXException {
+        if (qName.equalsIgnoreCase("page")) {
+            if (title.startsWith("Обсуждение портала:")
+                    || title.startsWith("Портал:")
+                    || title.startsWith("Обсуждение категории:")
+                    || title.startsWith("Категория:")
+                    || title.startsWith("Обсуждение справки:")
+                    || title.startsWith("Справка:")
+                    || title.startsWith("Обсуждение шаблона")
+                    || title.startsWith("Шаблон:")
+                    || title.startsWith("Обсуждение MediaWiki:")
+                    || title.startsWith("MediaWiki:")
+                    || title.startsWith("Обсуждение файла:")
+                    || title.startsWith("Файл:")
+                    || title.startsWith("Обсуждение Википедии:")
+                    || title.startsWith("Википедия:")
+                    || title.startsWith("Обсуждение участника:")
+                    || title.startsWith("Участник:")
+                    || title.startsWith("Обсуждение:")
+                    || title.startsWith("Служебная:")
+                    || title.startsWith("Медиа:"))
+                return;
+            write(title);
+            title = null;
+        } else if (qName.equalsIgnoreCase("title")) {
+            title = currentBuffer.toString();
+            currentBuffer = null;
+        } else if (qName.equalsIgnoreCase("text")) {
+            currentBuffer = null;
+        } else if (qName.equalsIgnoreCase("mediawiki")) {
+            close();
+        }
     }
+
     public void characters(char ch[],
-			   int start,
-			   int length)
-	throws SAXException {
-	if(currentBuffer==null) return;
-	currentBuffer.append(new String(ch, start, length));
+                           int start,
+                           int length)
+            throws SAXException {
+        if (currentBuffer == null) return;
+        currentBuffer.append(new String(ch, start, length));
     }
 }
