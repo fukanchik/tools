59476bf88f7bfe292670b19d94be311f83495ad3
Sergey Fukanchik <fukanchik@gmail.com>
2017-11-26 10:45:32 -0500

    Increase XML entity limit for Java 8

--- repack.sh
+++ repack.sh
@@ -1,4 +1,6 @@
 export CLASSPATH=target/wikitools-0.0.1.jar
 export JOPT="-Xms256m -Xmx512m -server -XX:+UseParallelGC"
+export JOPT="$JOPT -Djdk.xml.totalEntitySizeLimit=2147480000"
 
-java $JOPT ru.fuxx.Repack $*
+echo "java $JOPT ru.fuxx.Repack $1 $2 $3"
+java $JOPT ru.fuxx.Repack "$@"
